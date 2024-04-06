62016c88fca3cb963009248f18a03ff12864a657
Max <58262387+Pyr0maniac1@users.noreply.github.com>
2024-04-06 01:05:49 -0400

    Shuffle
    fisher-yates shuffle algo
    
    Co-Authored-By: Chase Colman <5411+chase@users.noreply.github.com>

--- Main.js
+++ Main.js
@@ -5,24 +5,39 @@ class Card {
     }
 
 
+
+}
+    class Deck {
+        constructor(){
+            this.Cards = [];  
+            this.reset(); 
+            this.shuffle(); 
+        }
+
+
+    reset(){ 
+        const suits = ['♠', '♣', '♥', '♦']; 
+        const values = ['2', '3', '4', '5', '6', '7 ', '8', '9', '10', 'J', 'Q', 'K', 'A']; 
+        
+        for(let suit of suits){ 
+            for(let value of values){ 
+                this.Cards.push(new Card(value, suit)); 
+            }
+        }
+    } 
+
+    shuffle(){
+        var i = this.Cards.length; 
+        while(i != 0){ 
+            var j = Math.floor(Math.random() * i);
+            i--; 
+            [this.Cards[i], this.Cards[j]] = [this.Cards[j], this.Cards[i]];  
+        }
+    };
+
+    deal(){
+        return this.Cards.pop();
+    }
 }
 
-class Deck {
-    constructor(){
-        this.Cards = [];  
-        this.reset(); 
-        this.shuffle(); 
-    }
-} 
-
-reset(){ 
-    const suits = ['♠', '♣', '♥', '♦']; 
-    const values = ['2', '3', '4', '5', '6', '7 ', '8', '9', '10', 'J', 'Q', 'K', 'A']; 
-    
-    for(let suit of suits){ 
-        for(let value of values){ 
-            this.Cards.push(new Card(value, suit)); 
-        }
-    }
-} 
 
