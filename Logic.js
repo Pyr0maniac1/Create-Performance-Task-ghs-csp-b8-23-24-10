ead16f9eda055ab637ba8462b826fd11db823cf6
Max <58262387+Pyr0maniac1@users.noreply.github.com>
2024-04-10 12:09:41 -0400

    Update Logic.js
    attempted to sort board, then sort the players hand by value, then get the highest value card

--- Logic.js
+++ Logic.js
@@ -1,21 +1,35 @@
 
-// points value 
-
-//sort 
-const sortedValues = 0; 
-
-// straight flush 
-// four of a kind 
-// full house 
-// flush  
-// straight 
-// three of a kind 
-// two pair 
-// one pair 
-// high card  
-
-check(cards) { 
-    const suits = cards.reduce((acc, card) => {}
+    class Logic {
+        constructor(points, checkedPlayer) {
+        // points value 
+        this.points = points
+        this.checkedPlayer = checkedPlayer
+        }
+        //sort 
+        sort() {
+        this.board = board.cards.value.sort()
+        this.player[x].cards = player[x].cards.value.sort()
+        console.log(this.board)
+        };
+        // straight flush 
+        straightFlush() {
         
-    );
-};
+        }
+        // four of a kind 
+        // full house 
+        // flush  
+        // straight 
+        // three of a kind 
+        // two pair 
+        // one pair 
+        // high card
+        highCard(player, checkedPlayer) {
+        return player[checkedPlayer].board.cards[0].value
+        }
+    
+        check() { 
+        this.sort()
+        
+        };
+    };
+    
